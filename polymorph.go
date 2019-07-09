c178ded2e66bfd542ba543895707f019698af796
Uasmi <uasmi.nasser@gmail.com>
2019-07-09 14:28:59 +0300

    Update polymorph.go

--- polymorph.go
+++ polymorph.go
@@ -4,8 +4,9 @@
 {{if in $record.Value "True"}}
 Oops, you're on cooldown. 
 {{else}}
-  You were polymorphed {{$usertag.Username}}!
-  :sheep:Baaah:sheep: 
+  {{$msg:= joinStr "" "You were polymorphed " $usertag.Username "!"}} 
+  {{sendMessage nil $msg}}
+  {{sendMessage nil ":sheep:Baaah:sheep: "}}
   {{giveRoleName $usertag "Sheep"}}
   {{takeRoleName $usertag "Sheep" (30)}}
   {{dbSetExpire .User.ID ("usedCC") ("True") (100)}}
