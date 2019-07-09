bfd054fe08ca3798d78d3c18ae4dacc3a8a14c39
Uasmi <uasmi.nasser@gmail.com>
2019-07-09 14:14:21 +0300

    Update polymorph.go

--- polymorph.go
+++ polymorph.go
@@ -2,7 +2,7 @@
 {{$record:= dbGet (.User.ID) ("usedCC")}}
 
 {{if in $record.Value "True"}}
-  Oops, you're on cooldown. 
+Oops, you're on cooldown. 
 {{else}}
   You were polymorphed {{$usertag.Username}}!
   :sheep:Baaah:sheep: 
@@ -13,40 +13,39 @@
   {{if targetHasRoleName $usertag "Mage"}}
     {{takeRoleName $usertag "Mage"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Mage"}}
+    {{giveRoleName $usertag "Mage"}}
   {{else if targetHasRoleName $usertag "Warrior"}}
     {{takeRoleName $usertag "Warrior"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Warrior"}}
+    {{giveRoleName $usertag "Warrior"}}
   {{else if targetHasRoleName $usertag "Warlock"}}
     {{takeRoleName $usertag "Warlock"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Warlock"}}
+    {{giveRoleName $usertag "Warlock"}}
   {{else if targetHasRoleName $usertag "Shaman"}}
     {{takeRoleName $usertag "Shaman"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Shaman"}}
+    {{giveRoleName $usertag "Shaman"}}
   {{else if targetHasRoleName $usertag "Rogue"}}
     {{takeRoleName $usertag "Rogue"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Rogue"}}
+    {{giveRoleName $usertag "Rogue"}}
   {{else if targetHasRoleName $usertag "Priest"}}
     {{takeRoleName $usertag "Priest"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Priset"}}
+    {{giveRoleName $usertag "Priset"}}
   {{else if targetHasRoleName $usertag "Paladin"}}
     {{takeRoleName $usertag "Paladin"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Paladin"}}
+    {{giveRoleName $usertag "Paladin"}}
   {{else if targetHasRoleName $usertag "Hunter"}}
     {{takeRoleName $usertag "Hunter"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Hunter"}}
+    {{giveRoleName $usertag "Hunter"}}
   {{else if targetHasRoleName $usertag "Druid"}}
     {{takeRoleName $usertag "Druid"}}
     {{sleep 30}}
-    {{giveRolename $usertag "Druid"}}
+    {{giveRoleName $usertag "Druid"}}
   {{end}}
   
 {{end}}
-
