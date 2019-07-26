5e727ccb836d0482627fbb40456c0b5b20c17959
Uasmi <uasmi.nasser@gmail.com>
2019-07-26 13:26:53 +0300

    Update polymorph.go

--- polymorph.go
+++ polymorph.go
@@ -1,4 +1,6 @@
-{{$usertag:= userArg (index .CmdArgs 0)}}
+{{$args:= parseArgs 1 "You have to mention someone!"
+  (carg "user" "usr")}}
+{{$usertag:=$args.Get 0}}
 {{$record:= dbGet (.User.ID) ("usedCC")}}
 
 {{if in $record.Value "True"}}
@@ -8,12 +10,14 @@ Oops, you're on cooldown.
     {{$msg:= joinStr "" $usertag.Username " is guarded by the Holy Spirit :sparkles:!"}}
     {{sendMessage nil $msg}}
   {{else}}
-    {{exec "sb" "sheep"}}
+    {{$cmd:=(exec "sb" "sheep")}}
+    {{/* $cmd */}}
     {{$msg:= joinStr "" "You were polymorphed " $usertag.Username "!"}} 
     {{sendMessage nil $msg}}
     {{sendMessage nil ":sheep:Baaah:sheep: "}}
     {{giveRoleName $usertag "Sheep"}}
     {{takeRoleName $usertag "Sheep" (30)}}
     {{dbSetExpire .User.ID ("usedCC") ("True") (10)}}
+    {{execCC 8 nil 0 (sdict "ID" $usertag.ID "Time" 30)}}
   {{end}}
 {{end}}
