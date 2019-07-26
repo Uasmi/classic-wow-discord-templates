257bd08b169b331737eee4ee9c371265c12d4352
Uasmi <uasmi.nasser@gmail.com>
2019-07-26 18:06:42 +0300

    Update dispel.go

--- dispel.go
+++ dispel.go
@@ -6,16 +6,16 @@
 {{if in $record.Value "True"}}
 Oops, you're on cooldown. 
 {{else}}
- {{exec "sb" "dispel"}}
-{{$msg:= joinStr "" "You're purified, " $usertag.Username "!"}} 
+  {{exec "sb" "dispel"}}
+  {{$msg:= joinStr "" "You're purified, " $usertag.Username "!"}} 
   {{sendMessage nil $msg}}
   {{sendMessage nil ":star2:"}}
-   {{if targetHasRoleName $usertag "Sheep"}}
+  {{if targetHasRoleName $usertag "Sheep"}}
       {{takeRoleName $usertag "Sheep"}}
-    {{else if targetHasRoleName $usertag "Banished"}}
+  {{else if targetHasRoleName $usertag "Banished"}}
       {{sendMessage nil "He's at the other realm... :smiling_imp:"}}
    {{else if targetHasRoleName $usertag "Stealth"}}
-  Hmmm, did you really see someone?
+      Hmmm, did you really see someone?
    {{end}}
    {{dbSetExpire .User.ID ("usedCC") ("True") (10)}}
 {{end}}
