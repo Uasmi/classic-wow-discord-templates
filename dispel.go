a861f4c3b0f7fed3bf3cf36fafe70edc72cce765
Uasmi <uasmi.nasser@gmail.com>
2019-07-26 18:05:52 +0300

    Create dispel.go

---
+++ dispel.go
@@ -0,0 +1,21 @@
+{{$args:= parseArgs 1 "You have to mention someone!"
+  (carg "user" "usr")}}
+{{$usertag:=$args.Get 0}}
+{{$record:= dbGet (.User.ID) ("usedCC")}}
+
+{{if in $record.Value "True"}}
+Oops, you're on cooldown. 
+{{else}}
+ {{exec "sb" "dispel"}}
+{{$msg:= joinStr "" "You're purified, " $usertag.Username "!"}} 
+  {{sendMessage nil $msg}}
+  {{sendMessage nil ":star2:"}}
+   {{if targetHasRoleName $usertag "Sheep"}}
+      {{takeRoleName $usertag "Sheep"}}
+    {{else if targetHasRoleName $usertag "Banished"}}
+      {{sendMessage nil "He's at the other realm... :smiling_imp:"}}
+   {{else if targetHasRoleName $usertag "Stealth"}}
+  Hmmm, did you really see someone?
+   {{end}}
+   {{dbSetExpire .User.ID ("usedCC") ("True") (10)}}
+{{end}}
