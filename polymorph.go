991d41043ae5de6c350facf79d6423cf58d642e1
Uasmi <uasmi.nasser@gmail.com>
2019-07-26 11:52:08 +0300

    Update polymorph.go
    removing roles script to separate CC

--- polymorph.go
+++ polymorph.go
@@ -15,42 +15,5 @@ Oops, you're on cooldown.
     {{giveRoleName $usertag "Sheep"}}
     {{takeRoleName $usertag "Sheep" (30)}}
     {{dbSetExpire .User.ID ("usedCC") ("True") (10)}}
-    {{if targetHasRoleName $usertag "Mage"}}
-      {{takeRoleName $usertag "Mage"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Mage"}}
-    {{else if targetHasRoleName $usertag "Warrior"}}
-      {{takeRoleName $usertag "Warrior"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Warrior"}}
-    {{else if targetHasRoleName $usertag "Warlock"}}
-      {{takeRoleName $usertag "Warlock"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Warlock"}}
-    {{else if targetHasRoleName $usertag "Shaman"}}
-      {{takeRoleName $usertag "Shaman"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Shaman"}}
-    {{else if targetHasRoleName $usertag "Rogue"}}
-      {{takeRoleName $usertag "Rogue"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Rogue"}}
-    {{else if targetHasRoleName $usertag "Priest"}}
-      {{takeRoleName $usertag "Priest"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Priest"}}
-    {{else if targetHasRoleName $usertag "Paladin"}}
-      {{takeRoleName $usertag "Paladin"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Paladin"}}
-    {{else if targetHasRoleName $usertag "Hunter"}}
-      {{takeRoleName $usertag "Hunter"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Hunter"}}
-    {{else if targetHasRoleName $usertag "Druid"}}
-      {{takeRoleName $usertag "Druid"}}
-      {{sleep 30}}
-      {{giveRoleName $usertag "Druid"}}
-    {{end}}
   {{end}}
 {{end}}
